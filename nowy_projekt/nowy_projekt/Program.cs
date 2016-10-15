c394ececb137b48879395b96f29b989d0eca87f8
Szymon Darmofał <Szymon Darmofa>
2016-10-15 07:46:01 +0200

    Przeciążenie funkcji

---
+++ nowy_projekt/nowy_projekt/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nowy_projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            opiszTyp(5);
+            opiszTyp("Hello world");
+            opiszTyp(5.0);
+            Console.ReadKey();
+        }
+
+        public static void opiszTyp(int wartosc)
+        {
+            Console.WriteLine("Typ int    || wartość: " + wartosc);
+        }
+        public static void opiszTyp(string wartosc)
+        {
+            Console.WriteLine("Typ string || wartość: " + wartosc);
+        }
+        public static void opiszTyp(double wartosc)
+        {
+            Console.WriteLine("Typ double || wartość: " + wartosc);
+        }
+    }
+}
